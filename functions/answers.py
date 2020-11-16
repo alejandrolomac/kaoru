862994b33e811b7690f7efe9e569df350fbbde65
reikrad <reikrad@gmail.com>
2020-11-15 19:30:06 -0600

    0.1

--- functions/answers.py
+++ functions/answers.py
@@ -1,16 +1,37 @@
+# -*- coding: UTF-8 -*-
 import webbrowser
 import speech_recognition as sr
 import pyaudio
+import json
+
+with open('functions/answers.json') as file:
+    ans = json.load(file)
+
+for dict in ans:
+    if dict['quest'].question == "hola":
+        print(dict['ans'][1].answer)
 
 def question_check(question):
     print("Haz dicho: {}".format(question))
     if "Amazon" in question:
         webbrowser.open('https://www.amazon.es')
+        answer = "Abriendo Amazon"
+    elif "Google" in question:
+        webbrowser.open('https://www.google.com')
+        answer = "Abriendo Google"
+    elif "Youtube" in question:
+        webbrowser.open('https://www.youtube.com')
+        answer = "Abriendo Youtube"
+    elif "Vendo en Casa" in question:
+        webbrowser.open('https://www.vendoencasa.net')
+        answer = "Abriendo Vendo en Casa"
     elif "Hola" in question or "hola" in question:
-        print("Hola, en que te puedo ayudar")
+        answer = "Hola, en que te puedo ayudar"
     elif "quien eres" in question or "quién eres" in question:
-        print("Soy Kaoru, una Inteligencia Aritificial")
-    elif "kaoru" in question:
-        print("Me llamo Kaoru, en honor a una de las artistas favoritas de mi creador, Kaouru Mori")
+        answer = "Soy Kaoru, una Inteligencia Artificial"
+    elif "kaoru" in question or "porque que te llamas asi" in question:
+        answer = "Me llamo Kaoru, en honor a una de las artistas favoritas de mi creador, Kaouru Mori"
     else:
-        print("no escuche bien")
+        answer = "no escuche bien, podrias repetirlo"
+
+    return answer
--- main.py
+++ main.py
@@ -1,19 +1,47 @@
+# -*- coding: UTF-8 -*-
+import pyttsx3
 import webbrowser
 import speech_recognition as sr
+from difflib import SequenceMatcher as SM
+import random
+import time
 import pyaudio
 
 from functions.answers import question_check
 
 r = sr.Recognizer()
+microphone = sr.Microphone(device_index = 0)
 
-while True:
-    with sr.Microphone() as source:
-        print("Hola, soy Kaoru")
+eng = pyttsx3.init()
+eng.setProperty("rate", 140)
+eng.setProperty("volume", 1.0)
+listVoices = eng.getProperty("voices")
+eng.setProperty("voice", listVoices[2].id)
+eng.say("Hola, soy Kaoru, en que te puedo ayudar")
+eng.runAndWait()
+
+print("Escuchando...")
+
+def recognizeMicAudio():
+    with microphone as source:
         r.adjust_for_ambient_noise(source)
-        audio = r.record(source, duration=3)
-        text = r.recognize_google(audio, language='es')
-        if text:
-            try:
-                question_check(text)
-            except:
-                print('No te he entendido')
+        audio = r.listen(source)
+        text = r.recognize_google(audio, language='es-ES')
+    
+    return text
+
+text = ""
+
+while text != "silencio":
+    text = recognizeMicAudio()
+    try:
+        eng.say(question_check(text))
+        eng.runAndWait()
+    except:
+        eng.say("No entendi bien lo que decias, podrias repetirmelo")
+        eng.runAndWait()
+
+
+#similitud = SM(None, entrada, palabra)
+#if similitud > 0.7;
+#   en.say(salida)
